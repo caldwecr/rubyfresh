dc2a5a1412d470b4d5d7a94ffc893d7e63f8699c
Courtland Caldwell <courtland.caldwell@dominodatalab.com>
2018-04-24 13:18:40 -0700

    Make input processing part of object
     It seems simpler to keep the functional code alongside
     the state, since we are working in Ruby

---
+++ submissions/queue_of_two_stacks_submission.rb
@@ -0,0 +1,56 @@
+# Implements a basic FIFO queue using two stacks
+class QueueOfTwoStacks
+  attr_reader :inbound
+  def initialize(items = [])
+    @inbound = items
+    @outbound = []
+  end
+
+  def enqueue(item)
+    @inbound << item
+  end
+
+  def dequeue
+    outbound.pop
+  end
+
+  def print_first
+    puts outbound.last
+  end
+
+  def outbound
+    maintain_invariant
+    @outbound
+  end
+
+  def self.process_input(input)
+    queries = input.drop 1 # Remove the number of queries from the input
+    queue = QueueOfTwoStacks.new
+    queries.each do |query|
+      query_parts = query.split
+      query_type = query_parts.first
+      case query_type
+      when '1'
+        queue.enqueue query_parts[1]
+      when '2'
+        queue.dequeue
+      when '3'
+        queue.print_first
+      else
+        raise ArgumentError, 'Unrecognized query type.'
+      end
+    end
+    queue
+  end
+
+  private def maintain_invariant
+    return unless @outbound.empty?
+    @outbound = @inbound.reverse
+    @inbound = []
+    nil
+  end
+end
+
+input = $stdin.readlines
+QueueOfTwoStacks.process_input input
+
